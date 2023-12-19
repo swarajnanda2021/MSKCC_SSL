ac2771ec94740ce4bc40ccd1a4088b254e1eb1c7
Swaraj Nanda <85413978+swarajnanda2021@users.noreply.github.com>
2023-12-19 05:12:02 +0000

    Update DataAug.py
    Added A DiNO data augmentation object (teacher, student multi-crop)

--- DataAug.py
+++ DataAug.py
@@ -1,6 +1,6 @@
 from torchvision import transforms
 
-
+# For SimCLR, NNCLR, and other invariance based methods
 class ContrastiveTransformations(object):
     def __init__(self, size=32, nviews=2, **kwargs):
         self.size = size
@@ -45,23 +45,56 @@ class ContrastiveTransformations(object):
         return [composed_transforms(x) for _ in range(self.nviews)]
 
 
+# For DiNO based non-invariance based methods:
+
+
+class DinoTransforms(object):
+    
+    def __init__(self, size, scale_teacher, scale_student,student_nviews, teacher_nviews):
+        self.transforms_teacher  = transforms.Compose([
+                transforms.RandomResizedCrop(size=size, scale = scale_teacher),
+                transforms.ToTensor(),
+                # Add any additional transformations if needed
+            ])
+        self.transforms_student  = transforms.Compose([
+                transforms.RandomResizedCrop(size=size, scale = scale_student),
+                transforms.ToTensor(),
+                # Add any additional transformations if needed
+            ]) 
+        self.student_nviews              = student_nviews
+        self.teacher_nviews              = teacher_nviews
+    
+    def __call__(self,x):
+
+        imgs_teacher = torch.stack([self.transforms_teacher(x) for _ in range(self.teacher_nviews)])
+        imgs_student = torch.stack([self.transforms_student(x) for _ in range(self.student_nviews)])
+
+        
+        return [imgs_student, imgs_teacher]# List [student, teacher] containing tensors: Batch, Views, Channel, Dim, Dim
+
+
+
 # Now place the usage example in a if __name__ == "__main__": block
 if __name__ == "__main__":
+    contrastive == 0
     # Initialize the ContrastiveTransformations object with custom parameters
-    contrastive_transform = ContrastiveTransformations(
-        nviews=2,
-        horizontal_flip=True,
-        resized_crop=True,
-        color_jitter=True,
-        random_grayscale=True,
-        brightness=0.5,
-        contrast=0.5,
-        saturation=0.5,
-        hue=0.1,
-        color_jitter_p=0.8,
-        grayscale_p=0.2,
-        to_tensor=True,
-        normalize=True,
-        mean=(0.5,),
-        std=(0.5,)
-    )
+    if contrastive == 1:
+        contrastive_transform = ContrastiveTransformations(
+            nviews=2,
+            horizontal_flip=True,
+            resized_crop=True,
+            color_jitter=True,
+            random_grayscale=True,
+            brightness=0.5,
+            contrast=0.5,
+            saturation=0.5,
+            hue=0.1,
+            color_jitter_p=0.8,
+            grayscale_p=0.2,
+            to_tensor=True,
+            normalize=True,
+            mean=(0.5,),
+            std=(0.5,)
+        )
+    else:
+        cifar_trainset = CIFAR10(root='./data', train= True, download=True, transform=DinoTransforms(size=32, scale_teacher= (0.95, 0.1), scale_student= (0.4, 0.5),teacher_nviews=3,student_nviews=1))
