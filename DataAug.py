f98ac308a2af6f32937fe6216e2bfad1c55d3168
Swaraj Nanda <85413978+swarajnanda2021@users.noreply.github.com>
2024-01-04 03:53:17 +0000

    Update DataAug.py
    Updated DiNO transforms to the correct version.

--- DataAug.py
+++ DataAug.py
@@ -50,31 +50,68 @@ class ContrastiveTransformations(object):
 # For DiNO based non-invariance based methods:
 
 
+# The original DinoTransforms wasn't great, the right version has 2 global crops and n (8 in OG implementation) local crops
+
 class DinoTransforms(object):
-    
-    def __init__(self, size, scale_teacher, scale_student,student_nviews, teacher_nviews):
-        self.transforms_teacher  = transforms.Compose([
-                transforms.Resize((size, size)),
-                transforms.RandomResizedCrop(size=size, scale = scale_teacher),
-                transforms.ToTensor(),
-                # Add any additional transformations if needed
-            ])
-        self.transforms_student  = transforms.Compose([
-                transforms.Resize((size, size)),
-                transforms.RandomResizedCrop(size=size, scale = scale_student),
-                transforms.ToTensor(),
-                # Add any additional transformations if needed
-            ]) 
-        self.student_nviews              = student_nviews
-        self.teacher_nviews              = teacher_nviews
-    
+
+
+    def __init__(self,
+                 local_size         = 96,
+                 global_size        = 224,
+                 local_crop_scale   = (0.05, 0.4),
+                 global_crop_scale  = (0.4, 1.0),
+                 n_local_crops      = 2,
+                 ):
+
+        self.n_local_crops = n_local_crops
+
+        # Assign some canonical transformations
+        flip_and_color_jitter = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+        ])
+
+        normalize = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+        self.global_1 = transforms.Compose([
+            transforms.RandomResizedCrop(size=global_size, scale = global_crop_scale),
+            transforms.RandomApply([flip_and_color_jitter], p=0.8),
+            transforms.GaussianBlur(3, (0.1,0.15)),
+            normalize,
+        ])
+
+        self.global_2 = transforms.Compose([
+            transforms.RandomResizedCrop(size=global_size, scale = global_crop_scale),
+            transforms.RandomApply([flip_and_color_jitter], p=0.8),
+            transforms.GaussianBlur(kernel_size = 3, sigma = (0.1,0.15)),
+            transforms.Lambda(lambda img: ImageOps.solarize(img, threshold=128)),
+            normalize,
+        ])
+
+        self.local    = transforms.Compose([
+            transforms.RandomResizedCrop(size=local_size, scale = local_crop_scale),
+            normalize,
+        ])
+
+
     def __call__(self,x):
+        crops = []
 
-        imgs_teacher = torch.stack([self.transforms_teacher(x) for _ in range(self.teacher_nviews)])
-        imgs_student = torch.stack([self.transforms_student(x) for _ in range(self.student_nviews)])
+        crops.append(self.global_1(x))
+        crops.append(self.global_2(x))
+        for _ in range(self.n_local_crops):
+            crops.append(self.local(x))
 
-        
-        return [imgs_student, imgs_teacher]# List [student, teacher] containing tensors: Batch, Views, Channel, Dim, Dim
+        return crops
+
+
 
 
 
@@ -105,12 +142,12 @@ if __name__ == "__main__":
         cifar_trainset  = CIFAR10(root='./data',train=True,download=True, transform=contrastive_transform)
     else:
         custom_transforms = DinoTransforms(
-            size=32, 
-            scale_teacher= (0.95, 0.1), 
-            scale_student= (0.4, 0.5),
-            teacher_nviews=3,
-            student_nviews=1
-        )
+                                  local_size         = 96,
+                                  global_size        = 224,
+                                  local_crop_scale   = (0.05, 0.4),
+                                  global_crop_scale  = (0.4, 1.0),
+                                  n_local_crops      = CROPS,
+                                  )
 
     
     # If you are working with the CIFAR10 dataset (for implementation), use the following line
