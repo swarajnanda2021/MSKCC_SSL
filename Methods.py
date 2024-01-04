62c7a723ab9bc726f3b3a15507bac275b57b958a
Swaraj Nanda <85413978+swarajnanda2021@users.noreply.github.com>
2024-01-04 03:59:39 +0000

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -542,26 +542,6 @@ class MAE(nn.Module):
 
 
 
-# Okay, now we are ready to write down the DiNO training method
-
-class CustomScheduler: # Need this else NaNs
-    def __init__(self, optimizer, warmup_epochs, initial_lr, final_lr, total_epochs):
-        self.optimizer = optimizer
-        self.warmup_epochs = warmup_epochs
-        self.initial_lr = initial_lr
-        self.final_lr = final_lr
-        self.total_epochs = total_epochs
-        self.after_warmup_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=total_epochs - warmup_epochs)
-
-    def step(self, epoch):
-        if epoch < self.warmup_epochs:
-            # Warm-up phase
-            lr = self.initial_lr + (self.final_lr - self.initial_lr) * epoch / self.warmup_epochs
-            for param_group in self.optimizer.param_groups:
-                param_group['lr'] = lr
-        else:
-            # After warm-up, use the cosine annealing schedule
-            self.after_warmup_scheduler.step(epoch - self.warmup_epochs)
 
 
 
