5367636a3dde5756e7594534bec66d482d95ba40
Swaraj Nanda <85413978+swarajnanda2021@users.noreply.github.com>
2024-02-10 07:50:58 +0000

    Update Encoders.py
    Introduced several modifications to resnet. It can be instantiated in the usual way, but there are several new flexibilities that are shown in Training.py. This includes:
    
    1) ResNext: grouped convolutions (cardinality of 32)
    2) Modifications to input branch by replacing one 7x7 conv with 3 3x3 convs.
    3) Modifications to the downsample module (both residual branch and the main branch), based on https://arxiv.org/pdf/1812.01187.pdf

--- Encoders.py
+++ Encoders.py
@@ -2,76 +2,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-# Encoder 0: Simple ConvNet
 
-class SimpleEncoder(nn.Module):
+# Encoder 1: ResNet with many modifications (incl. resnext)
 
-    # define a init class calling the cifar10 dataloader and only use one example
-    def __init__(self, hidden_dim):
-        super().__init__() # inherit parent class's attributes
-        self.latent_dim = hidden_dim
-        self.input_channels = 3
+class DownsampleModule(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, modification_type={''}):
+        super(DownsampleModule, self).__init__()
+        if 'resnetD' in modification_type: # add average pooling if resnetD is present
+            self.downsample = nn.Sequential(
+                nn.AvgPool2d(kernel_size=2, stride=2, padding=0),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        else: #just use the regular stuff # 'standard' in modification_type or 'resnetB' in modification_type:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+        
 
-        # define an encoder block for taking two images and feeding it to two encoders
-        self.encoder = nn.Sequential(
-            # 32 x 32 input
-        nn.Conv2d(in_channels=self.input_channels,out_channels=64,kernel_size=4,stride=1,padding=1),
-        nn.BatchNorm2d(64),
-        nn.LeakyReLU(0.2,inplace=False),
-        # Output size=[(Input size+2×padding−kernel size) / stride] + 1
-        # Conv2d will have out_channels x Output size x Output size
-        # which is (32 + 2*1 - 3)/1 + 1 = 32. So 64 x 32 x 32
-        nn.Conv2d(in_channels=64,out_channels=64,kernel_size=4,stride=2,padding=1),
-        nn.BatchNorm2d(64),
-        nn.LeakyReLU(0.2,inplace=False),
-        # now it is (32 + 2*1 - 4)/2 + 1 = 16, so 64 x 16 x 16
-        nn.Conv2d(in_channels=64,out_channels=128,kernel_size=4,stride=2,padding=1),
-        nn.BatchNorm2d(128),
-        nn.LeakyReLU(0.2,inplace=False),
-        # and now it should be 128 x 8 x 8
-        nn.Conv2d(in_channels=128,out_channels=256,kernel_size=4,stride=2,padding=1),
-        nn.BatchNorm2d(256),
-        nn.LeakyReLU(0.2,inplace=False),
-        # and now it should be 256 x 4 x 4
-        nn.Conv2d(in_channels=256,out_channels=4*self.latent_dim,kernel_size=4,stride=2,padding=1),
-        nn.BatchNorm2d(4*self.latent_dim),
-        nn.LeakyReLU(0.2,inplace=False),
-        # and now it should be 512 x 2 x 2
-        #nn.Conv2d(in_channels=512,out_channels=4*self.latent_dim,kernel_size=1,stride=1,padding=1),
-        #nn.BatchNorm2d(4*self.latent_dim),
-        #nn.LeakyReLU(0.2,inplace=False),
-        # and now it should be 4*self.latent_dim x 1 x 1
-        # This is the vector that is the compressed form of the 3*32*32 image to be fed to the projection
-        # head.
-        )
-
-        # define a multilayer perceptron block
-        self.MLP = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(4*self.latent_dim,self.latent_dim)
-        )
-
-
-    # define a forward function: this should produce the vector representation
-    def forward(self,x):
-        x = self.encoder(x)
-        x = self.MLP(x)
-        return x
-
-
-
-# Encoder 1: ResNet
-
+    def forward(self, x):
+        return self.downsample(x)
 
 class BasicBlock(nn.Module): # ResNet 18 and 34
     expansion = 1
 
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None, modification_type={''}):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        conv1_stride = 1
+        conv2_stride = stride
+        if 'resnetB' in modification_type and stride != 1:
+            conv1_stride = 1
+            conv2_stride = 2
+        if 'resnext' in modification_type: 
+            # if you want resnext we'll replace the 3x3 conv with a grouped conv with 32 groups
+            groups = 32
+        else:
+            groups = 1
+
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=conv1_stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride = conv2_stride,padding=1, bias=False, groups=groups)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
 
@@ -88,21 +61,33 @@ class BasicBlock(nn.Module): # ResNet 18 and 34
         if self.downsample is not None:
             identity = self.downsample(x)
 
+        print(identity.shape,out.shape)
+
         out += identity
         out = self.relu(out)
 
         return out
 
-
-# Bottleneck Block: ResNet 50, 101 and 152
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None, modification_type={''}):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        conv1_stride = 1
+        conv2_stride = stride
+        if 'resnetB' in modification_type and stride != 1:
+            conv1_stride = 1
+            conv2_stride = 2
+
+        if 'resnext' in modification_type: 
+            # if you want resnext we'll replace the 3x3 conv with a grouped conv with 32 groups
+            groups = 32
+        else:
+            groups = 1
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=conv1_stride, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=conv2_stride, padding=1,bias=False, groups = groups)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
@@ -124,76 +109,82 @@ class Bottleneck(nn.Module):
         out = self.bn3(out)
 
         if self.downsample is not None:
-            identity = self.downsample(x)
+            identity = self.downsample(identity)
+
+        print(identity.shape,out.shape)
 
         out += identity
         out = self.relu(out)
 
         return out
 
-# Convolution Block (Initial Layer)
-
-class ConvBlock(nn.Module):
-    def __init__(self,InputChannel,OutputChannel,Kernel,Padding,Stride):
-        super().__init__()
-        self.input_channel      = InputChannel
-        self.kernel             = Kernel
-        self.padding            = Padding
-        self.stride             = Stride
-        self.output_channel     = OutputChannel
-        self.convblock          = nn.Sequential(
-                nn.Conv2d(
-                        in_channels     =self.input_channel,
-                        out_channels    =self.output_channel,
-                        kernel_size     =self.kernel,
-                        stride          =self.stride,
-                        padding         =self.padding,
-                        bias            =False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size=self.kernel,stride=self.padding,padding=self.padding)
-        )
-
-    def forward(self,x):
-        x = self.convblock(x)
-        return x
-
-# ResNet Class
 class ResNet(nn.Module):
-    def __init__(self, block, layers, outputchannels=1000):
-        super().__init__()
+    def __init__(self, block, layers, outputchannels=1000, modification_type={''}):
+        super(ResNet, self).__init__()
         self.in_channels = 64
-        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(self.in_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, outputchannels)
 
-    def _make_layer(self, block, out_channels, blocks, stride=1):
+        # Input branch
+        if 'resnetC' in modification_type:
+          self.input_branch = nn.Sequential(
+              nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False),
+              nn.BatchNorm2d(32),
+              nn.ReLU(inplace=True),
+          
+              nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1, bias=False),
+              nn.BatchNorm2d(32),
+              nn.ReLU(inplace=True),
+          
+              nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+              nn.BatchNorm2d(64),
+              nn.ReLU(inplace=True),
+
+          )
+          
+        else:
+          self.input_branch = nn.Sequential(
+              nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+              nn.BatchNorm2d(64),
+              nn.ReLU(inplace=True),
+              
+          )
+        
+        # Maxpooling
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # layer 1 modifications (add resnext in layer1 (or stage 1) if asked)
+        if 'resnext' in modification_type:
+          layer1_mod = 'resnext'
+        else:
+          layer1_mod = ''
+
+        # Residual branch stages
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1, modification_type = {layer1_mod}) # no tweaks whatsoever to the first except if resnext is chosen
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, modification_type=modification_type)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, modification_type=modification_type)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, modification_type=modification_type)
+        
+        # Final average pool and fully connected linear layer
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1, modification_type={''}):
         downsample = None
-        if stride != 1 or self.in_channels != out_channels * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * block.expansion),
-            )
+        if stride != 1 or self.in_channels != out_channels * block.expansion: # only for the first layer
+            downsample = DownsampleModule(self.in_channels, out_channels * block.expansion, stride, modification_type)
 
         layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        layers.append(block(self.in_channels, out_channels, stride, downsample, modification_type))
         self.in_channels = out_channels * block.expansion
         for _ in range(1, blocks):
             layers.append(block(self.in_channels, out_channels))
 
         return nn.Sequential(*layers)
 
+
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        
+        x = self.input_branch(x)
+        
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -206,7 +197,6 @@ class ResNet(nn.Module):
         x = self.fc(x)
 
         return x
-
 
 
 # Encoder 2: ViT
